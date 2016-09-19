11c14c61db6109a0c5e484f5aa687d713859c46e
David Ganthier <david.ganthier@gmail.com>
2016-09-19 03:30:32 +0000

    Done.

--- index.js
+++ index.js
@@ -29,27 +29,3 @@ function addToCart(item) {
 }
 
 // Math.floor(Math.random(item) * 101)
-
-/*
-
-Original Code below: -->
-
-var cart;
-
-function setCart(newCart) {
-  cart = newCart;
-}
-
-function total() {
-  let t = 0
-
-  for (var i = 0, l = cart.length; i < l; i++) {
-    for (var item in cart[i]) {
-      t += cart[i][item]
-    }
-  }
-
-  return t
-}
-
-*/
